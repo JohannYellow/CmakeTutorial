f48f5ce3063afa4636d093de8d7169fef7152b73
Johann Kok <johann.kok@gmail.com>
2024-11-19 11:31:11 +0200

    Finished Step 2 Exercise 2.

--- Step2/tutorial.cxx
+++ Step2/tutorial.cxx
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // TODO 6: Replace sqrt with mathfunctions::sqrt
-
+  
   // calculate square root
   const double outputValue = mathfunctions::sqrt(inputValue);
   std::cout << "The square root of " << inputValue << " is " << outputValue
